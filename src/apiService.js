9f34538a9719d227b85ffb9aa83c252cd9551275
Iryna Zalata <iryna.zalata@gmail.com>
2020-11-02 22:21:01 +0200

    Add completed additional tasks

--- src/apiService.js
+++ src/apiService.js
@@ -1,4 +1,16 @@
+import {
+  error,
+  defaultModules,
+} from "../node_modules/@pnotify/core/dist/PNotify.js";
+import * as PNotifyDesktop from "../node_modules/@pnotify/desktop/dist/PNotifyDesktop.js";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import "../node_modules/@pnotify/desktop/dist/PNotifyDesktop.css";
+import '@pnotify/core/dist/BrightTheme.css';
+
+defaultModules.set(PNotifyDesktop, {});
+
 import imageTemplate from '../src/imageTemplate.hbs';
+// import * as InfiniteScroll from 'infinite-scroll';
 
 const ref = {
   TOKEN: '18931614-e34c06c7127746f74346ed387',
@@ -18,12 +30,14 @@ const ref = {
     fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${ref.page}&per_page=12&key=${ref.TOKEN}`)
       .then(response => response.json())
       .then(data => {
-        data.hits.forEach(el => document.querySelector('.gallery').insertAdjacentHTML('beforeend', `${imageTemplate(el)}`))
+        if (data.hits.length > 0) {
+          data.hits.forEach(el => document.querySelector('.gallery').insertAdjacentHTML('beforeend', `${imageTemplate(el)}`));
+          ref.loadMoreBtn.classList.remove('is-hidden')
+        } else error('Such images are not found')
         window.scrollTo({
           top: document.documentElement.offsetHeight,
           behavior: 'smooth'
         })
-          ref.loadMoreBtn.classList.remove('is-hidden');
       })
   },
 
@@ -32,5 +46,12 @@ const ref = {
     ref.renderImages(ref.form.query.value);
   },
 }
+
+// const elem = document.querySelector('.gallery');
+// const infScroll = new InfiniteScroll(elem, {
+//   path: ,
+//   append: '.photo-card',
+// // outlayer: instance,
+// });
 
 export default ref
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ref from './apiService.js';
 import * as basicLightbox from 'basiclightbox';
-import InfiniteScroll from 'infinite-scroll';
 
 import '../node_modules/basiclightbox/src/styles/main.scss';
 import './styles.css';
@@ -10,21 +9,10 @@ import './styles.css';
 ref.form.addEventListener('submit', ref.searchImage);
 ref.loadMoreBtn.addEventListener('click', ref.showMore);
 
-
 document.querySelector('.gallery').addEventListener('click', (event) => {
   basicLightbox.create(`
     <img width="1400" height="900" src="${event.target.dataset['name']}">
 	`).show(() => console.log('lightbox now visible'))
 })
 
-// infScroll.loadNextPage()
-const elem = document.querySelector('.gallery');
-const infScroll = new InfiniteScroll( elem, {
-  // options
-  path: function() {
-  const pageNumber = ( this.loadCount + 1 ) * 10;
-  return '/articles/P' + pageNumber;
-},
-  append: ref.showMore,
-  history: false,
-});
+
